d984b65e95676e443e1d1211d6a8fea59f8e6c1a
Guilherme Nunes <guilherme.augd@gmail.com>
2025-11-11 14:19:20 -0300

    imported function and component

--- components/gallery/ArtGallery.tsx
+++ components/gallery/ArtGallery.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
 import styles from './ArtGallery.module.css'
+import { fetchArtData } from '@/utils/utils'
+import Featured from '../featured/Featured'
+
 
 export default async function ArtGallery() {
+  const artData = await fetchArtData()
+
 
 
 }
