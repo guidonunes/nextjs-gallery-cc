d7e259ef43833ae159b7bcaed7aa355ea872fcaf
Guilherme Nunes <guilherme.augd@gmail.com>
2025-11-11 14:10:34 -0300

    import type and add image tag

--- components/featured/Featured.tsx
+++ components/featured/Featured.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import Image from 'next/image'
+import Artwork from '../../utils/utils'
 
 import styles from './Featured.module.css'
 
-export default function Featured() {
+
+
+
+export default function Featured( {objectID, primaryImage, title, artistDisplayName, objectDate}: Artwork ) {
 
   return (
     <div className={styles.featured}>
+      <Image
+        className={styles.featuredImage}
+        src={primaryImage == '' ? '/blank.jpg' : primaryImage}
+        alt={title}
+        width={500}
+        height={500}
+/>
 
     </div>
   );
