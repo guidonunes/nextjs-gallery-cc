a17cd3c87115de297e38a200c78b4cb586d95f5d
Guilherme Nunes <guilherme.augd@gmail.com>
2025-11-11 14:46:43 -0300

    import function to fetch data

--- components/comment/StoredComments.tsx
+++ components/comment/StoredComments.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from './Comment.module.css'
+import { fetchCommentData } from '../../utils/utils'
 
 export type StoredCommentsProps = {
   id: number
 }
 
 export default async function  StoredComments ({ id }: StoredCommentsProps) {
+  const commentData = await fetchCommentData(id);
 
 };
